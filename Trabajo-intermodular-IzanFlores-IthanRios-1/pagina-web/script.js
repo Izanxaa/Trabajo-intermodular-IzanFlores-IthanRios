29764347ca04fc4a80b10f40a5dc73ef06bce798
Izanxaa <157115424+Izanxaa@users.noreply.github.com>
2025-04-15 13:51:13 +0200

    Add files via upload

---
+++ Trabajo-intermodular-IzanFlores-IthanRios-1/pagina-web/script.js
@@ -0,0 +1,13 @@
+    const filtroInput = document.getElementById('filtro-productos');
+    if (filtroInput) {
+      filtroInput.addEventListener('input', () => {
+        const filtro = filtroInput.value.toLowerCase();
+        const productos = document.querySelectorAll('.producto');
+  
+        productos.forEach(p => {
+          const nombre = p.querySelector('h3').textContent.toLowerCase();
+          p.style.display = nombre.includes(filtro) ? 'block' : 'none';
+        });
+      });
+    };
+
